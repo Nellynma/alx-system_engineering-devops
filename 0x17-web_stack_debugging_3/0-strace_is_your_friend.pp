9660df28d2f2e90f1c2602da495ed3f047827483
Nellynma <nellyxtabel@gmail.com>
2023-06-07 11:38:10 -0700

    Web Stack Debugging 3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Replaces a line containing "phpp" with "php" in a file on a server.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
